a852615e07c7eef819fc219d26a09022c9591259
tibigameMarl <ryosukehashimoto1@gmail.com>
2023-10-10 16:44:38 +0900

    -vと-x POSTに相当する機能の実装

--- curl/src/Curl.java
+++ curl/src/Curl.java
@@ -11,16 +11,14 @@ import java.util.List;
 public class Curl {
 
     //curl https://example.com 相当のことができる機能
-    public void CurlHttp(){
+    public void CurlHttp(String requestUrl){
         System.out.println("curl https://example.com 相当のことができる機能");
-        String strUrl = "https://example.com";
-//		String strUrl = "http://httpbin.org/post";
         HttpURLConnection  urlConn = null;
         InputStream in = null;                      //結果を格納する
         BufferedReader reader = null;               //結果の読み込みを格納する
 
         try {
-            URL url = new URL(strUrl);                          //接続するURLを指定する
+            URL url = new URL(requestUrl);                          //接続するURLを指定する
             urlConn = (HttpURLConnection) url.openConnection(); //コネクションを取得する
             urlConn.setRequestMethod("GET");                    //GETメソッドをセット
             urlConn.connect();                                  //コネクト
@@ -52,17 +50,15 @@ public class Curl {
     }
 
     //curl -o file https://example.com 相当のことができる機能
-    public void CurlOFile(String fileName){
+    public void CurlOFile(String fileName,String requestUrl){
         System.out.println("curl -o file https://example.com 相当のことができる機能");
-        String strUrl = "https://example.com";
-//		String strUrl = "http://httpbin.org/post";
         HttpURLConnection  urlConn = null;
         InputStream in = null;                      //結果を格納する
         BufferedReader reader = null;               //結果の読み込みを格納する
         List<String> text = new ArrayList<>();
 
         try {
-            URL url = new URL(strUrl);                          //接続するURLを指定する
+            URL url = new URL(requestUrl);                          //接続するURLを指定する
             urlConn = (HttpURLConnection) url.openConnection(); //コネクションを取得する
             urlConn.setRequestMethod("GET");                    //GETメソッドをセット
             urlConn.connect();                                  //コネクト
@@ -97,13 +93,90 @@ public class Curl {
     }
 
     //curl -v  https://example.com 相当のことができる機能
-    public  void CurlVHttp(){
+    public  void CurlVHttp(String requestUrl){
         System.out.println("curl -v  https://example.com 相当のことができる機能");
+        HttpURLConnection  urlConn = null;
+        InputStream in = null;                      //結果を格納する
+        BufferedReader reader = null;               //結果の読み込みを格納する
+
+        try {
+            URL url = new URL(requestUrl);                          //接続するURLを指定する
+            urlConn = (HttpURLConnection) url.openConnection(); //コネクションを取得する
+            urlConn.setRequestMethod("GET");                    //GETメソッドをセット
+            urlConn.connect();                                  //コネクト
+            int status = urlConn.getResponseCode();             //応答されたコードを取得
+            System.out.println("HTTPステータス:" + status);       //応答された状態を表示、200なら成功
+
+            if (status == HttpURLConnection.HTTP_OK) {          //接続が成功している場合結果を読み込む
+                in = urlConn.getInputStream();                  //ストリームを取得
+                reader = new BufferedReader(new InputStreamReader(in)); //ストリームから文字列を読み取る
+                String line;                                    //readLine()の時の読み込んだ行を格納する
+                while ((line = reader.readLine()) != null) {
+                }
+            }
+
+            var headers = urlConn.getHeaderFields();
+            List<String> keyKist = new ArrayList<>(headers.keySet());               //ヘッダーのキーのリスト
+            for(int i=0;i < keyKist.size();i++){
+                System.out.print(keyKist.get(i) + ":");                             //ヘッダーのキーを表示
+                System.out.println(urlConn.getHeaderFields().get(keyKist.get(i)));  //ヘッダーの値を表示
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();                                //エラー箇所の表示
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();                             //リーダーを閉じる
+                }
+                if (urlConn != null) {
+                    urlConn.disconnect();                       //接続を切る
+                }
+            } catch (IOException e) {
+                e.printStackTrace();                            //エラー箇所を表示
+            }
+        }
+
+
     }
 
     //curl -X POST https://example.com 相当のことができる機能
-    public  void CurlXPost(){
+    public  void CurlXPost(String requestUrl){
         System.out.println("url -X POST https://example.com 相当のことができる機能");
+        HttpURLConnection  urlConn = null;
+        InputStream in = null;                      //結果を格納する
+        BufferedReader reader = null;               //結果の読み込みを格納する
+
+        try {
+            URL url = new URL(requestUrl);                          //接続するURLを指定する
+            urlConn = (HttpURLConnection) url.openConnection(); //コネクションを取得する
+            urlConn.setRequestMethod("POST");                    //GETメソッドをセット
+            urlConn.connect();                                  //コネクト
+            int status = urlConn.getResponseCode();             //応答されたコードを取得
+            System.out.println("HTTPステータス:" + status);       //応答された状態を表示、200なら成功
+
+            if (status == HttpURLConnection.HTTP_OK) {          //接続が成功している場合結果を読み込む
+                in = urlConn.getInputStream();                  //ストリームを取得
+                reader = new BufferedReader(new InputStreamReader(in)); //ストリームから文字列を読み取る
+                String line;                                    //readLine()の時の読み込んだ行を格納する
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);                   //読み込んだ行を表示する
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();                                //エラー箇所の表示
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();                             //リーダーを閉じる
+                }
+                if (urlConn != null) {
+                    urlConn.disconnect();                       //接続を切る
+                }
+            } catch (IOException e) {
+                e.printStackTrace();                            //エラー箇所を表示
+            }
+        }
     }
 
     //curl -X POST -d "key=value" https://example.com 相当のことができる機能
