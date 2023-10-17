699f86b40c73dfb6f1895ba58dd5b89845026b92
tibigameMarl <ryosukehashimoto1@gmail.com>
2023-10-17 13:35:28 +0900

    関数名の修正についてのリファクタリング

--- curl/src/CommandRun.java
+++ curl/src/CommandRun.java
@@ -1,30 +1,33 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 入力されたコマンドから求められている処理を整理するクラス
+ */
 public class CommandRun {
     public  String url;
     public List<String> commandList = new ArrayList<>();
     public  void run(){
         Curl curl = new Curl();
         CommandRun commandRun = new CommandRun();
-        if(commandList.isEmpty())
+        if(commandList.isEmpty())//curl https://example.com 相当のことができる機能
         {
             curl.CurlHttp(url);
         }
-        if(commandList.contains("-o")){
+        if(commandList.contains("-o")){//curl -o file https://example.com 相当のことができる機能
             String fileName;
             fileName = commandRun.getFileName(commandList);
             curl.isOutPutFile = true;
             curl.outPutFileName = fileName;
         }
-        if(commandList.contains("-v")){
+        if(commandList.contains("-v")){//curl -v  https://example.com 相当のことができる機能
             curl.isShowVersion = true;
         }
-        if(commandList.contains("-X") &&commandList.contains("POST"))
+        if(commandList.contains("-X") &&commandList.contains("POST"))//curl -X POST https://example.com 相当のことができる機能
         {
             curl.methodType = requestMethod.POST;
 
-            if(commandList.contains("-d")){
+            if(commandList.contains("-d")){//curl -X POST -d "key=value" https://example.com 相当のことができる機能
                 String value = "";
                 for (String s : commandList) {
                     if (s.startsWith("\"key=")) {
@@ -36,11 +39,12 @@ public class CommandRun {
                 curl.postText = value;
             }
         }
-
-
         curl.CurlHttp(url);
     }
 
+    /**
+     * 入力されたコマンドの中からファイル名に当たる部分を探す処理
+     */
     public  String getFileName(List<String> command)
     {
         String fileName = "temp";
